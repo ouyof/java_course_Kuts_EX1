ba62343a6c26f29a590effe767ff4d4f86e89bef
Kuts Nikita <kutcnikita9@gmail.com>
2022-04-08 11:48:00 +0300

    Initial commit

---
+++ src/lesson1_2/Main.java
@@ -0,0 +1,11 @@
+package lesson1_2;
+
+public class Main {
+    public static void main(String[] args) {
+        // Вариант 5
+        String name = "Auto";
+        Long money = 140_000_000L;
+        byte market = 42;
+        System.out.printf("Компания %s c годовым доходом %dр., имеет %d%c долю на рынке", name, money,market,37);
+    }
+}
